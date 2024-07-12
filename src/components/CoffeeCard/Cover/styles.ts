40ec0ffb7a5083cdd5f0c6267d4a7ba592dd4306
Gabriel Bursi <gabriellbursii@gmail.com>
2024-07-11 22:29:43 -0300

    style: border-radius Cover

--- src/components/CoffeeCard/Cover/styles.ts
+++ src/components/CoffeeCard/Cover/styles.ts
@@ -1,7 +1,8 @@
 import { styled, css } from 'styled-components/native'
 
 export const Cover = styled.Image`
-	${() => css`
+	${({ theme }) => css`
+		border-radius: ${theme.border.radius.big}px;
 		flex: 1;
 	`}
 `
